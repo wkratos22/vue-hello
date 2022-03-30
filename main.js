f2f05859ec9cf3804ac3a08af9492357b31150cc
wkratos22 <michaelwachnicki2001@gmail.com>
2022-03-30 17:42:15 +0200

    manca il bonus

---
+++ main.js
@@ -0,0 +1,11 @@
+var app = new Vue(
+    {
+        el: `#root`,
+        data: {
+            messaggio:`Hello World!`,
+            immagine:`./img/tyler-clemmensen-d1Jum1vVLew-unsplash.jpg`,
+           
+        },
+    }   
+);
+   
